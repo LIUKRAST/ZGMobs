f08a1f35ef68604da82fa360acef7ffd0aa08e0a
LiukRast <darthlucavader@gmail.com>
2025-05-20 14:52:07 +0200

    Removed unused statements

--- src/main/java/net/frozenblock/zgmobs/mixin/FinalizeSpawnMixin.java
+++ src/main/java/net/frozenblock/zgmobs/mixin/FinalizeSpawnMixin.java
@@ -1,14 +1,11 @@
 package net.frozenblock.zgmobs.mixin;
 
-import net.frozenblock.zgmobs.Config;
 import net.frozenblock.zgmobs.Germonium;
-import net.frozenblock.zgmobs.GermoniumUtils;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.monster.CaveSpider;
-import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.level.ServerLevelAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
